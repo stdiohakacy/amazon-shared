7910190844097ac6a4270f5910ed020098a888b2
Cornal Nguyen <duy.nguyend.tpv@one-line.com>
2025-06-03 17:56:54 +0700

    feat: 0.0.8

--- src/modules/response/interfaces/response.interface.ts
+++ src/modules/response/interfaces/response.interface.ts
@@ -1,3 +1,8 @@
 export interface ResponseMetadataInterface {
   [key: string]: unknown;
 }
+
+export interface ResponseInterface<T = void> {
+  _metadata?: ResponseMetadataInterface;
+  data?: T;
+}
